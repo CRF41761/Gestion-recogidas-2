2021b4d1618f34d2c6602319eced28a1cc1e10c4
CRF41761 <crf.granja.saler@gmail.com>
2025-04-07 14:33:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,37 @@
-const CACHE_NAME = "recogidas-cache-v1";
+const CACHE_NAME = "gestion-recogidas-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+    "/Gestion-recogidas-2/",  // Asegura que coincide con tu repositorio
+    "/Gestion-recogidas-2/index.html",
+    "/Gestion-recogidas-2/styles.css",
+    "/Gestion-recogidas-2/script.js",
+    "/Gestion-recogidas-2/manifest.json",
+    "/Gestion-recogidas-2/icons/icon-192x192.png",
+    "/Gestion-recogidas-2/icons/icon-512x512.png"
 ];
 
-// Instalación del Service Worker y almacenamiento en caché
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// Activación y limpieza de cachés antiguas
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
+    );
 });
 
-// Interceptar solicitudes y servir desde caché
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cache => cache !== CACHE_NAME).map(cache => caches.delete(cache))
+            );
+        })
+    );
+});
+

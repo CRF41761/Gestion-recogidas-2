01de226f708fd7d2cda1eff54ad1816b725ca70d
CRF41761 <crf.granja.saler@gmail.com>
2025-04-11 10:37:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = "gestion-recogidas-v1";
 const urlsToCache = [
-    "/Gestion-recogidas-2/",  
+    "/Gestion-recogidas-2/",  // Asegura que coincide con tu repositorio
     "/Gestion-recogidas-2/index.html",
     "/Gestion-recogidas-2/styles.css",
     "/Gestion-recogidas-2/script.js",
     "/Gestion-recogidas-2/manifest.json",
     "/Gestion-recogidas-2/icons/icon-192x192.png",
     "/Gestion-recogidas-2/icons/icon-512x512.png",
-    "/Gestion-recogidas-2/municipios.json"
+    "/Gestion-recogidas-2/municipios.json" // Archivo de municipios
 ];
 
 // Instalar el Service Worker y cachear archivos
@@ -39,4 +39,3 @@ self.addEventListener("activate", event => {
         })
     );
 });
-

6ddd55ba1e8d608517b07efa186f583596fc5dac
CRF41761 <crf.granja.saler@gmail.com>
2025-05-08 12:17:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", function () {
     mapElement.parentNode.insertBefore(locateButton, mapElement.nextSibling);
 
      // Generar automáticamente el número de entrada al cargar la página
-    fetch("https://script.google.com/macros/s/AKfycbwOl4PebKO72BeOZGo5DnjKaYISmYvtyJ7jj0hNF4l3Aw0_IpLgYq8V2JRyMI78ejk/exec?getNumeroEntrada") // Reemplaza con tu URL de Apps Script
+    fetch("https://script.google.com/macros/s/AKfycbz4GVrJFp6-exMr6lTSJM9akvjV_YH6dNxHSD4ZskLsy91SY4OluNuIVGTTQfCyTdNK/exec?getNumeroEntrada") // Reemplaza con tu URL de Apps Script
         .then(response => response.json())
         .then(data => {
             document.getElementById("numero_entrada").value = data.numero_entrada; // Asignar el número de entrada al formulario
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function enviarDatos(data) {
-        fetch("https://script.google.com/macros/s/AKfycbwOl4PebKO72BeOZGo5DnjKaYISmYvtyJ7jj0hNF4l3Aw0_IpLgYq8V2JRyMI78ejk/exec", {
+        fetch("https://script.google.com/macros/s/AKfycbz4GVrJFp6-exMr6lTSJM9akvjV_YH6dNxHSD4ZskLsy91SY4OluNuIVGTTQfCyTdNK/exec", {
             method: "POST",
             mode: "no-cors",  
             headers: {"Content-Type": "application/json"},

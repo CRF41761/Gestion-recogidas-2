e3e7fab1c2520afe62dec84e1e711b2ce214b719
CRF41761 <crf.granja.saler@gmail.com>
2025-12-19 12:56:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -327,7 +327,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const mapElement = document.getElementById("map");
     mapElement.parentNode.insertBefore(locateButton, mapElement.nextSibling);
 
-    fetch("https://script.google.com/macros/s/AKfycbxoDZH2RGMG5PkGEW4BGFrXlx3eFLqrM9R9eKBP6k9r4-8O8rWdajORvWgKwJClxqGy/exec?getNumeroEntrada")
+    fetch("https://script.google.com/macros/s/AKfycbzNAHg2SsWATcKJ8FtoYnbQx5ulWFuPmbkSA9yd58aosrKUs7jRrP6SGcKu_Rh2RSPC/exec?getNumeroEntrada")
         .then(r => r.json()).then(d => document.getElementById("numero_entrada").value = d.numero_entrada)
         .catch(console.error);
 
@@ -429,7 +429,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     async function enviarDatos(data, btn) {
         try {
-            await fetch("https://script.google.com/macros/s/AKfycbxoDZH2RGMG5PkGEW4BGFrXlx3eFLqrM9R9eKBP6k9r4-8O8rWdajORvWgKwJClxqGy/exec", {
+            await fetch("https://script.google.com/macros/s/AKfycbzNAHg2SsWATcKJ8FtoYnbQx5ulWFuPmbkSA9yd58aosrKUs7jRrP6SGcKu_Rh2RSPC/exec", {
                 method: "POST",
                 mode: "no-cors",
                 headers: { "Content-Type": "application/json" },
@@ -441,7 +441,7 @@ document.addEventListener("DOMContentLoaded", function () {
             } catch (dbError) {
                 console.error('Error guardando en IndexedDB:', dbError);
             }
-            const response = await fetch("https://script.google.com/macros/s/AKfycbxoDZH2RGMG5PkGEW4BGFrXlx3eFLqrM9R9eKBP6k9r4-8O8rWdajORvWgKwJClxqGy/exec?getNumeroEntrada");
+            const response = await fetch("https://script.google.com/macros/s/AKfycbzNAHg2SsWATcKJ8FtoYnbQx5ulWFuPmbkSA9yd58aosrKUs7jRrP6SGcKu_Rh2RSPC/exec?getNumeroEntrada");
             const d = await response.json();
             alert(` Número de entrada asignado: ${d.numeroEntrada}`);
             sessionStorage.setItem('formEnviadoOK', '1');
@@ -626,3 +626,4 @@ if (btnCerrar) {
 // Fecha actual por defecto
 const hoy = new Date().toISOString().split('T')[0];
 document.getElementById('fecha').value = hoy;
+

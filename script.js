13e0ed7583834125d940c428dd71b10e44ff9037
CRF41761 <crf.granja.saler@gmail.com>
2026-06-11 13:38:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -873,7 +873,7 @@ async function enviarDatos(data, btn) {
     await guardarRegistroLocalConEstado(registroPendiente, "pendiente");
 
     // 2. Enviar el formulario (sin leer respuesta, por no-cors)
-    await fetch("https://script.google.com/macros/s/AKfycbwsWOv7OoS9fjmrZO6V7w4uK8H2GhJr_WF8eVUC5qLWq0v0jCdisCjVVGEnOr8eWwPm/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbx4zZ7XBp-575WXGvSQYjKVoT4t4o56mkvVlozNq6cmxBb_dbdThFKNso9FWqMFhXi3/exec", {
       method: "POST",
       mode: "no-cors",
       headers: { "Content-Type": "application/json" },
@@ -884,7 +884,7 @@ async function enviarDatos(data, btn) {
     await new Promise(resolve => setTimeout(resolve, 500));
 
     // 4. Obtener todos los datos y filtrar/ordenar por número de entrada
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwsWOv7OoS9fjmrZO6V7w4uK8H2GhJr_WF8eVUC5qLWq0v0jCdisCjVVGEnOr8eWwPm/exec?funcion=getAllData", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbx4zZ7XBp-575WXGvSQYjKVoT4t4o56mkvVlozNq6cmxBb_dbdThFKNso9FWqMFhXi3/exec?funcion=getAllData", {
       method: "GET",
       mode: "cors"
     });
